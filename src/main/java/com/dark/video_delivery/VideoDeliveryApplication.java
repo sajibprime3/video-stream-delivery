91391a566124d59577a665b40d8a7364b6c58d1c
Sajib Ahmed <sajibprime3@gmail.com>
2025-10-22 20:57:13 +0600

    Spring boot init.

---
+++ src/main/java/com/dark/video_delivery/VideoDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.dark.video_delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dark/video_delivery/VideoDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dark.video_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
